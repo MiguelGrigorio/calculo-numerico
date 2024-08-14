ac01e6040728ced8443f65e042f2fa8b894586cc
MiguelGrigorio <miguelgrigorio@hotmail.com>
2024-08-14 14:29:07 -0300

    14/08 incompleto

---
+++ 4/main.jl
@@ -0,0 +1,45 @@
+using .MathConstants:e
+
+function fixedpoint(a, g, error)
+    x = g(a)
+    while abs(x - a) > error
+        a = x
+        x = g(a)
+    end
+    return x
+end
+
+function secant(a, b, f, error)
+    g(a, b) = (a*f(b)-b*f(a))/(f(b)-f(a))
+    x = g(a, b)
+    while abs(x - b) > error
+      a = b
+      b = x
+      x = g(a, b)
+    end
+    return x
+end
+
+function newton(a, f, f_derivado, error)
+    x = a - f(a)/f_derivado(a)
+    while abs(x - a) > error
+        a = x
+        x = a - f(a)/f_derivado(a)
+    end
+    return x
+end
+
+# Exercício 3.3.1
+g(x) = e^x - 2
+error = 10^(-8)
+
+r1 = fixedpoint(-1.8, g, error)
+println("Utilizando ponto fixo: $(round(r1, digits = 7))")
+
+# Exercício 3.4.1
+f(x) = cos(x) - x^2
+f1(x) = -sin(x) - 2*x
+error = 10^(-5)
+
+r2 = newton(1, f, f1, error)
+println("Utilizando Newton: $(round(r2, digits = 5))")
